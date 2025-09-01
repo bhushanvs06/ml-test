8d604e850c9d2b26e1474184fc0daef565886c0d
Bhushan vs <bhushanvs06@gmail.com>
2025-09-01 10:04:37 +0530

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import os
-
+//hiiiiiiii aniket
 # Load dataset
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
